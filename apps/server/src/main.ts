7d85d63176ad92b2c6bddc1406f464988e85c8e7
Lương Vĩ Phú <84316006+LVIPHU@users.noreply.github.com>
2024-03-06 16:42:23 +0700

    Feature/init backend (#1)
    * [feat] Added backend graphql and users feature
    
    * [feat] Added backend graphql and users feature

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,18 +5,29 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from './app/app.module';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  await app.register(helmet, {
+    contentSecurityPolicy: false,
+  });
+
+  app.enableCors();
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application playground is running on: http://localhost:${port}/graphiql`
   );
 }
 
-bootstrap();
+bootstrap().then(r => r);
---
+++ libs/server/feature/user/README.md
@@ -0,0 +1,7 @@
+# feature-user
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build feature-user` to build the library.
